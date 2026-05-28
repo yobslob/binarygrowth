810d0c22197a2eb17bddec5dbc1fd41246fe6848
yogesh <yogeshxiix@gmail.com>
2026-05-28 22:51:50 +0530

    updated rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,34 @@ import { withNextVideo } from "next-video/process";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/podcasting",
+        destination: "https://binary-podcasting.vercel.app/podcasting",
+      },
+      {
+        source: "/podcasting/:path*",
+        destination: "https://binary-podcasting.vercel.app/podcasting/:path*",
+      },
+      {
+        source: "/launch-videos",
+        destination: "https://binary-launch-videos.vercel.app/launch-videos",
+      },
+      {
+        source: "/launch-videos/:path*",
+        destination: "https://binary-launch-videos.vercel.app/launch-videos/:path*",
+      },
+      {
+        source: "/clipping",
+        destination: "https://binary-clipping.vercel.app/clipping",
+      },
+      {
+        source: "/clipping/:path*",
+        destination: "https://binary-clipping.vercel.app/clipping/:path*",
+      },
+    ];
+  },
 };
 
 export default withNextVideo(nextConfig);
